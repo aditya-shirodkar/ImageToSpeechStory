fec9881c59ebdb55382e207bb6dfe39d6998fbd2
Aditya <adityashirodkar2020@gmail.com>
2024-04-10 20:23:43 +0530

    first commit

---
+++ image_to_audio_story.py
@@ -0,0 +1,112 @@
+# converts provided text to an audio story
+# 1) uses Salesforce/blip-image-captioning-base (Huggingface) to caption image
+# 2) uses google Gemini to generate story on the above caption
+# 3) uses suno/bark-small (Huggingface) to create an audio file
+# 4) UI using streamlit
+# requires Huggingfacehub and google generative AI API tokens
+
+from api_keys import huggingfacehub_api_token, google_api_key
+
+from IPython.display import Audio
+from langchain.chains import LLMChain
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+import streamlit as st
+from transformers import pipeline
+
+import requests
+import os
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = huggingfacehub_api_token
+os.environ["GOOGLE_API_KEY"] = google_api_key
+
+
+# image-to-text
+# check huggingface.co/tasks for the source of the task name "image-to-text"
+def img_to_text(url):
+    image_to_text = pipeline(
+        task="image-to-text",
+        model="Salesforce/blip-image-captioning-base",
+        max_new_tokens=200,
+    )
+    text = image_to_text(url)[0]["generated_text"]
+    print(text)
+    return text
+
+
+# llm
+def generate_story(scenario):
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=google_api_key)
+    template = """
+    You are a storyteller;
+    you can generate a short story based on a simple narrative;
+    the story should be no more than 50 words long;
+    
+    Context: {scenario}
+    Story:"""
+
+    prompt = PromptTemplate(template=template, input_variables=["scenario"])
+    llm = LLMChain(
+        llm=llm,
+        prompt=prompt,
+        verbose=True,
+    )
+    story = llm.invoke(scenario)
+    print(story["text"])
+    return story["text"]
+
+
+# text-to-speech
+def text_to_speech(message):
+    txt_to_speech = pipeline(task="text-to-speech", model="suno/bark-small")
+    speech = txt_to_speech(message)
+    print(speech)
+
+    sound = Audio(speech["audio"], rate=speech["sampling_rate"])
+    with open("output/audio.flac", "wb") as file:
+        file.write(sound.data)
+
+
+# alternatively, implementing using the Inference API
+def text_to_speech2(message):
+    API_URL = "https://api-inference.huggingface.co/models/suno/bark-small"
+    headers = {"Authorization": f"Bearer {huggingfacehub_api_token}"}
+    payloads = {"text_inputs": message}
+
+    response = requests.post(API_URL, headers=headers, json=payloads)
+    print(response)
+
+    with open("output/audio.mp4", "wb") as file:
+        file.write(response.content)
+
+
+# scenario = img_to_text("images/machine-learning-1.jpg")
+# story = generate_story(scenario)
+# text_to_speech(story)
+
+
+# UI on streamlit
+def main():
+    st.set_page_config(page_title="Image to audio story", page_icon="🎧")
+    st.header("Convert an image into an audio story")
+    image_file = st.file_uploader("Choose an image", type="jpg")
+
+    if image_file is not None:
+        print(image_file)
+        image_bytes = image_file.getvalue()
+        with open(image_file.name, "wb") as file:
+            file.write(image_bytes)
+        scenario = img_to_text(image_file.name)
+        story = generate_story(scenario)
+        text_to_speech(story)
+
+        with st.expander("scenario"):
+            st.write(scenario)
+        with st.expander("story"):
+            st.write(story)
+
+        st.audio("output/audio.flac")
+
+
+if __name__ == "__main__":
+    main()
